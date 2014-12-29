5829db1b82cfa7f3d4e83b28a2c760d73355636d
Michael Traw <sgv@o2.pl>
2014-12-29 11:14:57 +0100

    Update demo

--- js/Game.js
+++ js/Game.js
@@ -70,7 +70,6 @@ kf.Game.prototype.onSettingsChanged = function()
 };
 kf.Game.prototype.onHideSettings = function()
 {
- this.onSettingsChanged();
  this.display.hideSettings();
  this.suspended = false;
  if (!this.over && !this.paused)
